9b22127aa65bacdec1debd6cd5a1991a3b78c653
Valerio De Filippo <valerio.defilippo@gmail.com>
2022-02-22 13:59:58 +0100

    m

---
+++ src/components/ProfileImage.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function ProfileImage({image}) {
+    return (
+        <div>
+            <img src={image} className="profile" alt="profile"/>
+
+        </div>
+    )
+}
---
+++ src/components/Timestamp.js
@@ -0,0 +1,11 @@
+
+   
+import React from 'react'
+
+export default function Timestamp({time}) {
+    return (
+        <div>
+            <span className="timestamp">{time}</span>
+        </div>
+    )
+}
